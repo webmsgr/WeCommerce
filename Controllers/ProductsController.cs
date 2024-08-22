df2984058317ff6a458eb175efceb68932b5861c
webmsgr <zapcontact000@gmail.com>
2024-08-22 11:36:43 -0700

    make add product page functional

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WeCommerce.Data;
+using WeCommerce.Models;
 
 namespace WeCommerce.Controllers
 {
-    public class ProductsController : Controller
-    {
+    public class ProductsController : Controller {
+
+        private readonly WeCommerceContext _context;
+
+        public ProductsController(WeCommerceContext context)
+        {
+            _context = context;
+        }
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                // add to DB
+                _context.Products.Add(product);
+                _context.SaveChanges();
+                // show success message
+                ViewData["message"] = "Product added successfully";
+                return View();
+            }
+
+            return View(product);
         }
     }
 }
