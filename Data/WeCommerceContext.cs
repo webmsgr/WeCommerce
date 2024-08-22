13abc7a7f07aa36c2cfebcf8d65a8760a121a986
webmsgr <zapcontact000@gmail.com>
2024-08-22 10:58:37 -0700

    Create context

---
+++ Data/WeCommerceContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeCommerce.Models;
+
+namespace WeCommerce.Data
+{
+    public class WeCommerceContext : DbContext
+    {
+        public WeCommerceContext(DbContextOptions<WeCommerceContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
