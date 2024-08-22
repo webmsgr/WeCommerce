d5b52a1cf1abdabfe41ca374be8a5a90964cc777
Wackery <32279125+webmsgr@users.noreply.github.com>
2024-08-22 11:40:49 -0700

    Create create product page (#14)
    * create controller and view for creating products
    
    * make add product page functional
    
    * Make it async

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeCommerce.Data;
+using WeCommerce.Models;
+
+namespace WeCommerce.Controllers
+{
+    public class ProductsController : Controller {
+
+        private readonly WeCommerceContext _context;
+
+        public ProductsController(WeCommerceContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                // add to DB
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+                // show success message
+                ViewData["message"] = "Product added successfully";
+                return View();
+            }
+
+            return View(product);
+        }
+    }
+}
