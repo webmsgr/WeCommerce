fb88f1d088904d634af2c1e0dee69a72f96a2c31
Wackery <32279125+webmsgr@users.noreply.github.com>
2024-08-26 10:08:59 -0700

    Add Pagination (#26)
    * yoo is that pagination???
    
    * add next and previous buttons
    
    * use page size of 10

---
+++ Models/ProductCatalogPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WeCommerce.Models
+{
+    public class ProductCatalogPageViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public int PageCount { get; set; }
+        public int CurrentPage { get; set; }
+    }
+}
