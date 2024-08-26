f9582306d089db972732bb1e63cf2a26ad6484e9
Wackery <32279125+webmsgr@users.noreply.github.com>
2024-08-26 10:37:48 -0700

    Add change password and profile (#33)

---
+++ Models/UserProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WeCommerce.Models
+{
+    public class UserProfileViewModel
+    {
+        public User User { get; set; }
+
+        public UserChangePassword ChangePassword { get; set; }
+
+    }
+}
