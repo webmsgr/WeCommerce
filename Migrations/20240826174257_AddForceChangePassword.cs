5f36c6e50fc15ca405502c0c7424ceb1ae4da912
webmsgr <zapcontact000@gmail.com>
2024-08-26 10:43:33 -0700

    add force change password field

---
+++ Migrations/20240826174257_AddForceChangePassword.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeCommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddForceChangePassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ForceChangePassword",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ForceChangePassword",
+                table: "Users");
+        }
+    }
+}
